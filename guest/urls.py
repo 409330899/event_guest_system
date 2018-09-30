578942b5f59a9a4c24f6f3105cc29b58a1b76bbc
409330899 <409330899@qq.com>
2018-09-30 20:41:39 +0800

    event_guest_system

---
+++ guest/urls.py
@@ -0,0 +1,40 @@
+"""guest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from sign import views
+from sign import urls
+from sign import urls as sign_url
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^api_test/$', views.api_test),
+    url(r'^index/$', views.index),
+    url(r'^logout/$', views.logout),
+    url(r'^login_action/$', views.login_action),
+    url(r'^event_manage/$', views.event_manage),
+    url(r'^guest_manage/$', views.guest_manage),
+    url(r'^accounts/login/$', views.index),
+    url(r'^search_name/$', views.search_name),
+    url(r'^search_phone/$', views.search_phone),
+    url(r'^sign_index/(?P<event_id>[0-9]+)/$', views.sign_index),
+    url(r'^sign_index2/(?P<event_id>[0-9]+)/$', views.sign_index2),
+    url(r'^sign_index_action/(?P<event_id>[0-9]+)/$', views.sign_index_action),
+    url(r'^api/', include((sign_url, 'sign'), namespace='sign')),
+    url(r'^api/test/$', views.api_test_practise),
+]
